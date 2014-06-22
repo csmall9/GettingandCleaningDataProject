ef57b46ca88fdd078d8eb7163b17b652ca843162
csmall9 <csmall9@verizon.net>
2014-06-22 16:52:31 -0400

    This is an near-final version of the script

--- run_analysis.R
+++ run_analysis.R
@@ -1,8 +1,8 @@
-## This the the file that can run to achieve the Project Assignment
-## 
+## Project for Getting and Cleaning Data
+
 ## Merges the training and the test sets to create one data set.
   
-  ## This portion of the script combines the training data  
+## This portion of the script combines the training data  
   
   ##  Step 1 - Read in test data (x_test.txt, y_test.txt, and subject_test.txt)
   
@@ -55,6 +55,8 @@ for (i in 1:nrow(y_test)){
     y_test[i,] <- "LAYING"
   }
 }
+
+## Now the the "test" data is tidy,  combine the test files
 testdataframe <- cbind(y_test, subject_test, testdata)
 head(testdataframe, 1)
 
@@ -69,7 +71,8 @@ head(y_train)
 subject_train <- read.table("subject_train.txt")
 head(subject_train)
 
-##  Add column names to test data,  read column name information from file.
+##  Add column names to training data,  read column name information from file.
+##  Note:  you do not have to read the features file again, but just in case the script in not run all togeher
 
 features <- read.table ("features.txt")
 features
@@ -109,7 +112,10 @@ for (i in 1:nrow(y_train)){
     y_train[i,] <- "LAYING"
   }
 }
-trainingtdataframe <- cbind(y_train, subject_train, trainingdata)
+
+## Combine the training data
+  
+trainingdataframe <- cbind(y_train, subject_train, trainingdata)
 head(trainingdataframe, 1)
 
 ## Now combine the test data with the training data
@@ -121,15 +127,46 @@ alldataframe <- rbind(testdataframe, trainingdataframe)
 
   extractCols <- grep("-mean\\(\\)|-std\\(\\)", names(alldataframe))
   extractCols
-  nmeanstd <- length(extractCols)
+  
+## Now extract the mean() and std() columns and combine together in an new dataframe 
 
+ncolumns <-length(extractCols)
+colnums <- 0  
+for(i in 1:ncolumns){
+  colnums <- colnums +1
+  newcolumn <- alldataframe[extractCols[i]]
+  if (colnums == 1){
+  print(names(alldataframe[extractCols[i]]))
+  extractdataframe <- newcolumn
+  }
+  else{
+  extractdataframe <- cbind(extractdataframe, newcolumn)
+  } 
+}
+head(extractdataframe, 3)
+  
+## prior to writing the file, add the test activity and subject colums
 
+  test_activity <- alldataframe$"test_activity"
+  subject_identifier <- alldataframe$"subject_identifier"
+  columns <- cbind.data.frame(testactivity, subject)
+  names(columns) <- c("test_activity", "subject_identifier")
+  head(columns,3)  
 
-### reshape
+  extractdataframe <- cbind.data.frame(columns, extractdataframe)
+  head(extractdataframe,2)
+  
+## Write the extracted data file -- 
 
-*************************************************  
-  ## executed from "projectdata" directory
-  ## copy all files to the "projectdata" direcory
+write.table(extractdataframe, file = "projectdataExtract2-tidy.txt", append = FALSE, quote = FALSE, sep = " ",
+            eol = "\n", na = "NA", dec = ".", row.names = FALSE,
+            col.names = TRUE, qmethod = c("escape", "double"),
+            fileEncoding = "")
+
+### Need to generate the independent tidy file
+
+#################################################################  
+
   
   
   extractdataframe <- alldataframe[1]  #test_activityextractdataframe <- cbind(extractdataframe, alldataframe$"subject_identifier")
@@ -137,15 +174,57 @@ alldataframe <- rbind(testdataframe, trainingdataframe)
   length(extractdataframe)
   extractdataframe <- cbind(extractdataframe, alldataframe[2])   ##"subject_identifier")
 
+activitysplit <- split(extractdataframe, extractdataframe$test_activity)
+
+names(activitysplit)
+      activitysplit[[1]]$"fBodyAccJerk-std()-X"
+
+mean1 <- mean(activitysplit[[1]]$"fBodyAccJerk-std()-X")
+
+
   
   
 #####
 
-Try "-mean\\(\\)"
-ncolumns <-length(extractCols)
-for(i in 1:ncolumns){
-  print(names(alldataframe[extractCols[i]]))
-  newcolumn[i] <- alldataframe[extractCols[i]]
-  extractdataframe <- cbind(extractdataframe, newcolumn[i])
-}  
+
+  
+  ## combine extracted variables with test and subject columns
+  
+  testactivity <- alldataframe$"test_activity"
+  subject <- alldataframe$"subject_identifier"
+  columns <- cbind(testactivity, subject)
+  head(columns,3)
+  
 head(extractdataframe, 3)
+
+write.table(extractdataframe, file = "projectdataExtract-tidy.txt", append = FALSE, quote = FALSE, sep = " ",
+            eol = "\n", na = "NA", dec = ".", row.names = FALSE,
+            col.names = TRUE, qmethod = c("escape", "double"),
+            fileEncoding = "")
+
+length(activitysplit)
+
+## Final activity is to generate an independent tidy file that calculates
+## mean for the variables by activity and subject.  
+
+mean1<- mean(activitysplit[1]$"fBodyBodyAccJerkMag-mean()")
+newframe <- tapply(extractdataframe$"fBodyBodyGyroJerkMag-std()", extractdataframe$"test_activity",mean)
+
+rownumber <- 0
+for (i in 3:68){
+  rowinframe <- tapply(extractdataframe[,i], extractdataframe$"test_activity",mean)
+  rownumber<- rownumber +1
+  if (i == 3) { 
+      totalnewframe <- rowinframe
+  }
+  else{
+    totalnewframe <- rbind(totalnewframe, rowinframe)
+  }
+} 
+rownames(totalnewframe) <- allcolumns[3:68]
+
+write.table(totalnewframe, file = "tidy_data-averages_by_activity.txt", append = FALSE, quote = FALSE, sep = " ",
+            eol = "\n", na = "NA", dec = ".", row.names = TRUE,
+            col.names = TRUE, qmethod = c("escape", "double"),
+            fileEncoding = "")
+e
